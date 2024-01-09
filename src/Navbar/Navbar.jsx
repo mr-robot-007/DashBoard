840493b7b2addc088a816e7a21d2c335f93f86f0
mr-robot-007 <anujgusain108@gmail.com>
2024-01-09 13:03:18 +0530

    height issues fixed

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import Wallet from "./Wallet";
 
 function Navbar() {
   return (
-    <div className="sticky  row-span-full  h-full max-h-[94rem] flex w-[14rem] flex-col justify-between overflow-y-hidden   bg-[#13233d]   text-white ">
+    <div className="sticky  row-span-full  h-full max-h-[94.2rem] flex w-[14rem] flex-col justify-between overflow-y-hidden   bg-[#13233d]   text-white ">
       <div className="  w-[14rem] sm:fixed relative h-full ">
         <Nav />
         <Wallet />
