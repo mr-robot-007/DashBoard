0b13f8f15ca8d0706ff308529ca4a95a3d13b6ff
mr-robot-007 <anujgusain108@gmail.com>
2024-01-09 13:02:02 +0530

    responsive issues fixed

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import Wallet from "./Wallet";
 
 function Navbar() {
   return (
-    <div className="sticky  row-span-full   flex w-[14rem] flex-col justify-between overflow-y-hidden   bg-[#13233d]   text-white ">
-      <div className=" fixed h-[100dvh]  w-[14rem] ">
-          <Nav />
-          <Wallet />
+    <div className="sticky  row-span-full  h-full max-h-[94rem] flex w-[14rem] flex-col justify-between overflow-y-hidden   bg-[#13233d]   text-white ">
+      <div className="  w-[14rem] sm:fixed relative h-full ">
+        <Nav />
+        <Wallet />
       </div>
     </div>
   );
