bb8a82a13cd9553f0ef6047eeb2e2a0142c0341f
mr-robot-007 <anujgusain108@gmail.com>
2024-01-09 12:13:14 +0530

    navbar position fixed

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -3,9 +3,11 @@ import Wallet from "./Wallet";
 
 function Navbar() {
   return (
-    <div className=" row-span-full flex flex-col  justify-between bg-[#13233d] text-white   overflow-y-hidden   w-[14rem] ">
-      <Nav />
-      <Wallet />
+    <div className="  row-span-full  flex w-[14rem] flex-col justify-between overflow-y-hidden   bg-[#13233d]   text-white ">
+      <div className="fixed h-full  w-[14rem] ">
+          <Nav />
+          <Wallet />
+      </div>
     </div>
   );
 }
